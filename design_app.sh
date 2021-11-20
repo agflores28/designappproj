44b6b83d31fac4e849d907cb7931a67b7f9a3679
NetAcad DEVASC <changeme@example.com>
2021-11-20 03:04:45 +0000

    new docker

--- design_app.sh
+++ design_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-mkdir tempdir
-mkdir tempdir/templates
-mkdir tempdir/static
+mkdir -p tempdir
+mkdir -p tempdir/templates
+mkdir -p tempdir/static
 
 cp designapp.py tempdir/.
 cp -r templates/* tempdir/templates/.
